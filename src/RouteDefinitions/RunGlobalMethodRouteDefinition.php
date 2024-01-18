0264acb83ab1b12d1360f59ac57ff170d678bb38
pjordaan <pjordaan@users.noreply.github.com>
2024-01-18 11:05:29 +0000

    Update file(s) "packages/rest-api/." from "apie-lib/apie-lib-monorepo"

--- src/RouteDefinitions/RunGlobalMethodRouteDefinition.php
+++ src/RouteDefinitions/RunGlobalMethodRouteDefinition.php
@@ -4,6 +4,8 @@ namespace Apie\RestApi\RouteDefinitions;
 use Apie\Common\ActionDefinitions\ActionDefinitionInterface;
 use Apie\Common\ActionDefinitions\RunGlobalMethodDefinition;
 use Apie\Common\Actions\RunAction;
+use Apie\Common\Concerns\ReadsRouteAttribute;
+use Apie\Core\Attributes\Route;
 use Apie\Core\BoundedContext\BoundedContextId;
 use Apie\Core\Enums\RequestMethod;
 use Apie\Core\ValueObjects\UrlRouteDefinition;
@@ -11,6 +13,10 @@ use ReflectionMethod;
 
 class RunGlobalMethodRouteDefinition extends AbstractRestApiRouteDefinition
 {
+    use ReadsRouteAttribute;
+
+    private const CURRENT_TARGET = Route::API;
+
     public function __construct(ReflectionMethod $method, BoundedContextId $boundedContextId)
     {
         parent::__construct($method->getDeclaringClass(), $boundedContextId, $method);
@@ -18,11 +24,19 @@ class RunGlobalMethodRouteDefinition extends AbstractRestApiRouteDefinition
 
     public function getMethod(): RequestMethod
     {
+        $route = $this->getRouteAttribute();
+        if ($route && $route->requestMethod) {
+            return $route->requestMethod;
+        }
         return empty($this->method->getParameters()) ? RequestMethod::GET : RequestMethod::POST;
     }
 
     public function getUrl(): UrlRouteDefinition
     {
+        $route = $this->getRouteAttribute();
+        if ($route) {
+            return new UrlRouteDefinition($route->routeDefinition);
+        }
         $methodName = $this->method->getName();
         if ($methodName === '__invoke') {
             return new UrlRouteDefinition($this->method->getDeclaringClass()->getShortName());
